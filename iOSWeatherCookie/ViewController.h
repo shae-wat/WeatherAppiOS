0ed8f6d65c70241096cdee34f445747abb91b39a
Shaelyn <shaelynjoy@gmail.com>
2016-01-28 18:12:26 -0800

    Weather icons

---
+++ iOSWeatherCookie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSWeatherCookie
+//
+//  Created by Shaelyn Watson on 1/28/16.
+//  Copyright © 2016 Shaelyn Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSWeatherCookie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSWeatherCookie
+//
+//  Created by Shaelyn Watson on 1/28/16.
+//  Copyright © 2016 Shaelyn Watson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
